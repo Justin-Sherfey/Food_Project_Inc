1729ffec81b423588b872ba36a9bf50b493f305f
brett <brett.beattie99@gmail.com>
2020-07-11 17:29:20 -0700

    featured page files added

---
+++ MealMeister/MealMeister/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  Project_Food
+//
+//  Created by Brett Beattie on 7/8/20.
+//  Copyright © 2020 Brett Beattie. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Shmeckle()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
